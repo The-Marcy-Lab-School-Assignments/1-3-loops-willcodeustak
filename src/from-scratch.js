997468f3b6fc3a486bd8d97623245eb5600cb730
WilliamZeng <itswilliamzeng@gmail.com>
2024-04-09 18:12:40 -0400

    loop finished

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,21 +1,50 @@
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
 };
 
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++) {
+    console.log(i);
+  }
 };
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i += 2) {
+    console.log(i);
+  }
 };
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--) {
+    console.log(i);
+  }
 };
 
-const loopUpToNum = () => {
+const loopUpToNum = (num) => {
+  for (let i = 0; i < num; i++) {
+    console.log(i);
+  }
 };
 
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++)
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log(`fizzbuzz`);
+    } else if (i % 3 === 0) {
+      console.log(`fizz`);
+    } else if (i % 5 === 0) {
+      console.log(`buzz`);
+    } else {
+      console.log(i);
+    }
 };
+console.log(fizzbuzz());
+//forgot "else" if
+//forgot to hoist the && to the top
+//forgot to call "fizzbuzzz() at the end"
 
 module.exports = {
   loop0UpTo10,
